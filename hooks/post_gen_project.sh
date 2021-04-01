de416cb47e45634101a00288b418860be54b25cb
Robert Kaussow <mail@geeklabor.de>
2021-04-01 14:35:36 +0200

    chore: keep master as default branch for now

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 if hash git 2>/dev/null; then
-    git init
+    git init -b master
     git remote add origin ssh://git@{{ cookiecutter.git_server }}/{{ cookiecutter.git_org }}/{{ cookiecutter.image_name }}.git
 fi
